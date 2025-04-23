cb6baf04182b47e46e9e5c08cefed1b60e3ba098
MrZeKron <cidm3965@gmail.com>
2025-04-23 19:03:20 -0400

    Actualización de templates y urls

--- Web/urls.py
+++ Web/urls.py
@@ -3,17 +3,10 @@ from App import views  # Importa correctamente el módulo views
 
 urlpatterns = [
     path('registrar-dispositivo/', views.registrar_dispositivo, name='registrar_dispositivo'),
-    path('registrar-tanque/', views.registrar_tanque_agua, name='registrar_tanque_agua'),
+    path('registrar-tanque/', views.registrar_tanque_agua, name='registrar_tanque'),
     path('registrar-usuario/', views.registrar_usuario, name='registrar_usuario'),
+    path('registro-usuario/', views.registrar_usuario, name='registro_usuario'),
     path('iniciar-sesion/', views.iniciar_sesion, name='iniciar_sesion'),
-    path('registro-usuario/', views.registro_usuario, name='registro_usuario'),
-    path('registrar-dispositivo/', views.registrar_dispositivo, name='registrar_dispositivo'),
-    path('registrar-tanque/', views.registrar_tanque, name='registrar_tanque'),
-
-
     path('menu/', views.menu, name='menu'),
-
-
-
     path('', views.inicio, name='inicio'),  # Vista de inicio
 ]
